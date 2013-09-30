uranusjr/Qelly

commit ad17070883e903e3aca3e74b007112ec05034bc0

Fix bug where pasting an empty string crashes app

src/View.cpp
@@ -372,6 +372,8 @@ void View::insertText(const QString &string, uint delayMs)
         }
     }
 
+    if (bytes.isEmpty())
+        return;
     if (!delayMs)
         emit hasBytesToSend(bytes);
     else

commit 2e2583c53cece8ecc6e7baef2efa9814ce6b87af

Fix #25: Paste function is broken
Introduced in commit ad1707088.

The text-insertion uses a QQueue instead of a plain QByteArray to
store the pending characters when between-character delay is enabled.
In such cases, therefore, the `bytes` variable will be empty (because
the `insertBuffer` instance variable is used instead).

The previous change naively checks for emptiness of `bytes` before
commting the insertion. While this does prevent crashing if `bytes`
is empty, it breaks situations in which `bytes` is empty, but
`inserBuffer` is not. The new implementation takes care of both
situations.

I'm feeling that having two kinds of insersion container is kind of
messy. This may need some refactoring in the future.

src/View.cpp
@@ -376,12 +376,16 @@ void View::insertText(const QString &string, uint delayMs)
         }
     }
 
-    if (bytes.isEmpty())
-        return;
     if (!delayMs)
-        emit hasBytesToSend(bytes);
+    {
+        if (!bytes.isEmpty())
+            emit hasBytesToSend(bytes);
+    }
     else
-        d->insertTimer->start(delayMs);
+    {
+        if (!d->insertBuffer.isEmpty())
+            d->insertTimer->start(delayMs);
+    }
 }
 
 void View::popInsertBuffer()